da6f4a2d73daa7d5794f6acacc6d9328ff30e39c
wbsly <qlyfdljh@163.com>
2022-07-18 22:49:29 +0800

    实现了关注按钮

---
+++ myspace/src/components/UserProfilePosts.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="card">
+        <div class="card-body">
+            <div v-for="post in posts.posts" :key="post.id">
+               {{ post.content}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "UserProfilePosts",
+    props: {
+        posts: {
+            type: Object,
+            required: true,
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
--- myspace/src/views/UserProfileView.vue
+++ myspace/src/views/UserProfileView.vue
@@ -2,22 +2,77 @@
     <ContentBase>
         <div class="row">
             <div class="col-3">
-                <UserProfileInfo />
+                <UserProfileInfo @follow="follow" @unfollow="unfollow" :user="user"/>
             </div>
-            <div class="col-9">帖子列表</div>
+            <div class="col-9">
+                <UserProfilePosts :posts="posts"/>
+            </div>
         </div>
     </ContentBase>
 </template>
 
 <script>
+import { reactive } from 'vue';
 import ContentBase from '../components/ContentBase';
 import UserProfileInfo from '../components/UserProfileInfo';
+import UserProfilePosts from '../components/UserProfilePosts';
 
 export default {
     name: 'UserProfileView',
     components: {
         ContentBase,
-        UserProfileInfo
+        UserProfileInfo,
+        UserProfilePosts,
+    },
+
+    setup() {
+        const user = reactive({
+            username: "lijiahao",
+            lastName: "Li",
+            firstName: "Jiahao",
+            followerCount: 0,
+            is_followed: false,
+        });
+
+        const posts = reactive({
+            count: 3,
+            posts: [
+                {
+                    id: 1,
+                    userId: 1,
+                    content: "测试1",
+                },
+                {
+                    id: 2,
+                    userId: 1,
+                    content: "测试2",
+                },
+                {
+                    id: 3,
+                    userId: 1,
+                    content: "测试3",
+                },
+            ]
+        });
+        
+        const follow = () => {
+            if (user.is_followed) return;
+            user.is_followed = true;
+            user.followerCount ++ ;
+        };
+
+        const unfollow = () => {
+            if (!user.is_followed) return;
+            user.is_followed = false;
+            user.followerCount -- ;
+        }
+
+        return {
+            user: user, // 如果类似这样key和value的值得一样可以简写为user
+            follow,
+            unfollow,
+            posts,
+        }
     }
 }
 </script>
